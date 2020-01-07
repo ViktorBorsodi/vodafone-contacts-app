5c3f47e5d1f9042217c6de66179ea4905846f7ee
Viktor Borsodi <borsodiviktor@gmail.com>
2020-01-07 16:15:28 +0100

    Updates

---
+++ precache-manifest.d9f3ad4de01ca081743456175ede31cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dba2618920f919770288e2b4d883e0c1",
+    "url": "/vodafone-contacts-app/index.html"
+  },
+  {
+    "revision": "b95551a6f5c2926d8d03",
+    "url": "/vodafone-contacts-app/static/css/main.0778dc18.chunk.css"
+  },
+  {
+    "revision": "c54febe802e229d40406",
+    "url": "/vodafone-contacts-app/static/js/2.cc188bc4.chunk.js"
+  },
+  {
+    "revision": "b95551a6f5c2926d8d03",
+    "url": "/vodafone-contacts-app/static/js/main.e708aec0.chunk.js"
+  },
+  {
+    "revision": "54cc99cb1dff35cc8382",
+    "url": "/vodafone-contacts-app/static/js/runtime-main.fe7224b3.js"
+  },
+  {
+    "revision": "f5331cb6372b6c0d8baf2dd7e200498c",
+    "url": "/vodafone-contacts-app/static/media/OpenSans-Bold.f5331cb6.ttf"
+  },
+  {
+    "revision": "d7d5d4588a9f50c99264bc12e4892a7c",
+    "url": "/vodafone-contacts-app/static/media/OpenSans-Regular.d7d5d458.ttf"
+  },
+  {
+    "revision": "b52e9e45b7b0f3ce28bb49f627b82f53",
+    "url": "/vodafone-contacts-app/static/media/vodafone_logo.b52e9e45.svg"
+  }
+]);
